4ee128ab9479d37b272661f2768f27a66d114fc4
Dimitar Yosifov <dimitaryosifov@outlook.com>
2019-01-06 04:07:05 +0200

    Heroku test

--- config/prod.exs
+++ config/prod.exs
@@ -11,6 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :layertwo, LayertwoWeb.Endpoint,
   load_from_system_env: true,
+  http: [port: {:system, "PORT"}],
   url: [scheme: "https", host: System.get_env("HOST_NAME"), port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
